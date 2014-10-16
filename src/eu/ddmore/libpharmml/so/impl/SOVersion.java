83e462c1fb5f9791aa0b6c9b497ed0b69920dacd
Florent Yvon <florent@ebi.ac.uk>
2014-10-16 17:57:36 +0100

    Added corresponding PharmML version

--- src/eu/ddmore/libpharmml/so/impl/SOVersion.java
+++ src/eu/ddmore/libpharmml/so/impl/SOVersion.java
@@ -1,17 +1,21 @@
 package eu.ddmore.libpharmml.so.impl;
 
+import eu.ddmore.libpharmml.impl.PharmMLVersion;
+
 public enum SOVersion {
 	
-	v0_1("0.1","SOMarshaller.xmlCatalogLocation.0.1");
+	v0_1("0.1","SOMarshaller.xmlCatalogLocation.0.1",PharmMLVersion.V0_4);
 	
 	public static final SOVersion DEFAULT = v0_1;
 	
 	private String version;
 	private String catalogLocation;
+	private PharmMLVersion pharmmlVersion;
 	
-	private SOVersion(String version,String catalogMessage){
+	private SOVersion(String version,String catalogMessage,PharmMLVersion pharmmlVersion){
 		this.version = version;
 		this.catalogLocation = Messages.getString(catalogMessage);
+		this.pharmmlVersion = pharmmlVersion;
 	}
 	
 	@Override
@@ -29,6 +33,10 @@ public enum SOVersion {
 
 	public String getCatalogLocation() {
 		return catalogLocation;
+	}
+	
+	public PharmMLVersion getCorrespondingPharmMLVersion(){
+		return pharmmlVersion;
 	}
 
 }
