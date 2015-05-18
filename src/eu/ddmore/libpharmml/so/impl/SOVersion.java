79b6635ae99d8f12fa57c0f2159311330a101658
Florent Yvon <florent@ebi.ac.uk>
2015-05-18 16:56:12 +0100

    fixed catalog location

--- src/eu/ddmore/libpharmml/so/impl/SOVersion.java
+++ src/eu/ddmore/libpharmml/so/impl/SOVersion.java
@@ -24,9 +24,9 @@ public enum SOVersion {
 	
 	v0_0("0.0","SOMarshaller.xmlCatalogLocation.0.0",PharmMLVersion.V0_4),
 	v0_1("0.1","SOMarshaller.xmlCatalogLocation.0.1",PharmMLVersion.V0_6),
-	v0_2("0.2","SOMarshaller.xmlCatalogLocation.0.1",PharmMLVersion.V0_6);
+	v0_2("0.2","SOMarshaller.xmlCatalogLocation.0.2",PharmMLVersion.V0_6);
 	
-	public static final SOVersion DEFAULT = v0_0;
+	public static final SOVersion DEFAULT = v0_2;
 	
 	private String version;
 	private String catalogLocation;
