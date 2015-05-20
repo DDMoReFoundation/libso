055258ac82cbc1fee3a77dee3a2e909385b59552
Florent Yvon <florent@ebi.ac.uk>
2015-05-20 10:41:06 +0100

    explicit call of AfterUnmarshal

--- src/eu/ddmore/libpharmml/so/impl/SOUnmarshalListener.java
+++ src/eu/ddmore/libpharmml/so/impl/SOUnmarshalListener.java
@@ -5,10 +5,15 @@ import eu.ddmore.libpharmml.impl.UnmarshalListener;
 public class SOUnmarshalListener extends UnmarshalListener {
 	
 	SOVersion soVersion;
-
+	
 	public SOUnmarshalListener(SOVersion version){
 		super(version.getCorrespondingPharmMLVersion());
 		this.soVersion = version;
+	}
+	
+	@Override
+	public void afterUnmarshal(Object target, Object parent) {
+		super.afterUnmarshal(target, parent);
 	}
 	
 }
