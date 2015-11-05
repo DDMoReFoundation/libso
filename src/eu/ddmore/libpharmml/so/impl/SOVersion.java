37517b30a65926773dc0012d0cd8a5dd953f4055
Florent Yvon <florent@ebi.ac.uk>
2015-11-05 11:33:10 +0000

    set default PharmML version to 0.6.1

--- src/eu/ddmore/libpharmml/so/impl/SOVersion.java
+++ src/eu/ddmore/libpharmml/so/impl/SOVersion.java
@@ -24,7 +24,7 @@ public enum SOVersion {
 	
 	v0_0("0.0","SOMarshaller.xmlCatalogLocation.0.0",PharmMLVersion.V0_4),
 	v0_1("0.1","SOMarshaller.xmlCatalogLocation.0.1",PharmMLVersion.V0_6),
-	v0_2("0.2","SOMarshaller.xmlCatalogLocation.0.2",PharmMLVersion.V0_6);
+	v0_2("0.2","SOMarshaller.xmlCatalogLocation.0.2",PharmMLVersion.V0_6_1);
 	
 	public static final SOVersion DEFAULT = v0_2;
 	
