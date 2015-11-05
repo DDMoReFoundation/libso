d8e80a8cfee0d2ffccceef77c5458d77d680b844
Florent Yvon <florent@ebi.ac.uk>
2015-11-05 11:36:38 +0000

    + logger message about versions used

--- src/eu/ddmore/libpharmml/so/impl/SOXMLFilter.java
+++ src/eu/ddmore/libpharmml/so/impl/SOXMLFilter.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 European Molecular Biology Laboratory,
+ * Heidelberg, Germany.
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of
+ * the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ *******************************************************************************/
 package eu.ddmore.libpharmml.so.impl;
 
 import java.io.BufferedReader;
@@ -15,6 +33,7 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import eu.ddmore.libpharmml.impl.LoggerWrapper;
 import eu.ddmore.libpharmml.impl.PharmMLVersion;
 import eu.ddmore.libpharmml.impl.XMLFilter;
 
@@ -29,6 +48,7 @@ public class SOXMLFilter extends XMLFilter {
 	
 	public SOXMLFilter(SOVersion writtenVersion){
 		super(writtenVersion.getCorrespondingPharmMLVersion());
+		LoggerWrapper.getLogger().warning("Filtering namespaces of SO "+writtenVersion+" and PharmML "+writtenVersion.getCorrespondingPharmMLVersion().getValue());
 		this.pharmml_version = writtenVersion.getCorrespondingPharmMLVersion();
 		this.so_version = writtenVersion;
 		NS_DOC_SO = getSONamespace(so_version);
@@ -40,7 +60,7 @@ public class SOXMLFilter extends XMLFilter {
     	OutputStreamWriter osw = new OutputStreamWriter(os);
     	while((line = br.readLine()) != null){
     		line = line.replaceAll("xmlns(:\\w+)?=\"http://www.pharmml.org/[^\"(so)]+/([^\"]+)\"",
-    				"xmlns$1=\"http://www.pharmml.org/pharmml/"+ pharmml_version.toString() +"/$2\"");
+    				"xmlns$1=\"http://www.pharmml.org/pharmml/"+ pharmml_version.getURIVersion() +"/$2\"");
     		osw.append(line);
     		osw.append("\n");
     	}
@@ -55,102 +75,127 @@ public class SOXMLFilter extends XMLFilter {
     		
     		XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(is);
 
+			@Override
 			public Object getProperty(String name) throws IllegalArgumentException {
 				return reader.getProperty(name);
 			}
 
+			@Override
 			public int next() throws XMLStreamException {
 				return reader.next();
 			}
 
+			@Override
 			public void require(int type, String namespaceURI, String localName) throws XMLStreamException {
 				reader.require(type, filterInputNamespace(namespaceURI), localName);
 			}
 
+			@Override
 			public String getElementText() throws XMLStreamException {
 				return reader.getElementText();
 			}
 
+			@Override
 			public int nextTag() throws XMLStreamException {
 				return reader.nextTag();
 			}
 
+			@Override
 			public boolean hasNext() throws XMLStreamException {
 				return reader.hasNext();
 			}
 
+			@Override
 			public void close() throws XMLStreamException {
 				reader.close();
 			}
 
+			@Override
 			public String getNamespaceURI(String prefix) {
 				return filterInputNamespace(reader.getNamespaceURI(prefix));
 			}
 
+			@Override
 			public boolean isStartElement() {
 				return reader.isStartElement();
 			}
 
+			@Override
 			public boolean isEndElement() {
 				return reader.isEndElement();
 			}
 
+			@Override
 			public boolean isCharacters() {
 				return reader.isCharacters();
 			}
 
+			@Override
 			public boolean isWhiteSpace() {
 				return reader.isWhiteSpace();
 			}
 
+			@Override
 			public String getAttributeValue(String namespaceURI, String localName) {
 				return reader.getAttributeValue(filterInputNamespace(namespaceURI), localName);
 			}
 
+			@Override
 			public int getAttributeCount() {
 				return reader.getAttributeCount();
 			}
 
+			@Override
 			public QName getAttributeName(int index) {
 				return reader.getAttributeName(index);
 			}
 
+			@Override
 			public String getAttributeNamespace(int index) {
 				return filterInputNamespace(reader.getAttributeNamespace(index));
 			}
 
+			@Override
 			public String getAttributeLocalName(int index) {
 				return reader.getAttributeLocalName(index);
 			}
 
+			@Override
 			public String getAttributePrefix(int index) {
 				return reader.getAttributePrefix(index);
 			}
 
+			@Override
 			public String getAttributeType(int index) {
 				return reader.getAttributeType(index);
 			}
 
+			@Override
 			public String getAttributeValue(int index) {
 				return reader.getAttributeValue(index);
 			}
 
+			@Override
 			public boolean isAttributeSpecified(int index) {
 				return reader.isAttributeSpecified(index);
 			}
 
+			@Override
 			public int getNamespaceCount() {
 				return reader.getNamespaceCount();
 			}
 
+			@Override
 			public String getNamespacePrefix(int index) {
 				return reader.getNamespacePrefix(index);
 			}
 
+			@Override
 			public String getNamespaceURI(int index) {
 				return filterInputNamespace(reader.getNamespaceURI(index));
 			}
 
+			@Override
 			public NamespaceContext getNamespaceContext() {
 				final NamespaceContext context = reader.getNamespaceContext();
 				return new NamespaceContext() {
@@ -172,43 +217,53 @@ public class SOXMLFilter extends XMLFilter {
 				};
 			}
 
+			@Override
 			public int getEventType() {
 				return reader.getEventType();
 			}
 
+			@Override
 			public String getText() {
 				return reader.getText();
 			}
 
+			@Override
 			public char[] getTextCharacters() {
 				return reader.getTextCharacters();
 			}
 
+			@Override
 			public int getTextCharacters(int sourceStart, char[] target, int targetStart, int length)
 					throws XMLStreamException {
 				return reader.getTextCharacters(sourceStart, target, targetStart, length);
 			}
 
+			@Override
 			public int getTextStart() {
 				return reader.getTextStart();
 			}
 
+			@Override
 			public int getTextLength() {
 				return reader.getTextLength();
 			}
 
+			@Override
 			public String getEncoding() {
 				return reader.getEncoding();
 			}
 
+			@Override
 			public boolean hasText() {
 				return reader.hasText();
 			}
 
+			@Override
 			public Location getLocation() {
 				return reader.getLocation();
 			}
 
+			@Override
 			public QName getName() {
 				QName qname = reader.getName();
 				QName newQname = new QName(filterInputNamespace(qname.getNamespaceURI()), 
@@ -217,42 +272,52 @@ public class SOXMLFilter extends XMLFilter {
 				return newQname;
 			}
 
+			@Override
 			public String getLocalName() {
 				return reader.getLocalName();
 			}
 
+			@Override
 			public boolean hasName() {
 				return reader.hasName();
 			}
 
+			@Override
 			public String getNamespaceURI() {
 				return filterInputNamespace(reader.getNamespaceURI());
 			}
 
+			@Override
 			public String getPrefix() {
 				return reader.getPrefix();
 			}
 
+			@Override
 			public String getVersion() {
 				return reader.getVersion();
 			}
 
+			@Override
 			public boolean isStandalone() {
 				return reader.isStandalone();
 			}
 
+			@Override
 			public boolean standaloneSet() {
 				return reader.standaloneSet();
 			}
 
+			@Override
 			public String getCharacterEncodingScheme() {
 				return reader.getCharacterEncodingScheme();
 			}
 
+			@Override
 			public String getPITarget() {
 				return reader.getPITarget();
 			}
 
+			@Override
 			public String getPIData() {
 				return reader.getPIData();
 			}
